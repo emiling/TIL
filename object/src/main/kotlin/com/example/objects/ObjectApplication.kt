0dfd951a65c0b06ac2d0153a5317a452e3197623
eugene.lee <eugene.lee@navercorp.com>
2022-07-03 23:23:46 +0900

    init: 오브젝트 예제 프로젝트 초기화

---
+++ object/src/main/kotlin/com/example/objects/ObjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.objects
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ObjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<ObjectApplication>(*args)
+}
